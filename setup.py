5ee8de20e188184da66ab74e480ed655e3ac0684
doffing81 <doffing81@gmail.com>
2021-01-14 15:06:29 -0700

    Added files.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lambdata_bcd", # Replace with your own username
+    version="0.1",
+    author="Brett",
+    description="A Lambda School assignment",
+    url="https://github.com/doffing81/lambdata",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
